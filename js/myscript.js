11afdd92ad49e5a2ac430f6edcbfcd8a8aeb2b7a
mustafiz-tech <102187767+mustafiz-tech@users.noreply.github.com>
2022-07-30 01:49:28 +0530

    2nd change

--- js/myscript.js
+++ js/myscript.js
@@ -2,3 +2,19 @@
 $('.slider-main').slick({
     dots: true,
 });
+
+$('.testominal-main').slick({
+    dots: false,
+    slidesToShow: 2,
+    infinite: false,
+    dots: true,
+    arrows: false,
+});
+
+$('.clients-main').slick({
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
