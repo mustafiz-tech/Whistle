fe65c6fe9d4828ffa2b710142e00bfc387fe424a
mustafiz-tech <102187767+mustafiz-tech@users.noreply.github.com>
2022-08-04 21:34:40 +0530

    change

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,8 @@
 // slick 
 $('.slider-main').slick({
     dots: true,
+    prevArrow: '<i class="fa  fa-arrow-left left-arrows">',
+    nextArrow: '<i class="fa  fa-arrow-right right-arrows">',
 });
 
 $('.testominal-main').slick({
@@ -18,3 +20,9 @@ $('.clients-main').slick({
     autoplay: true,
     autoplaySpeed: 2000,
 });
+
+$(function(){
+    $('.nav-section').click(function(){
+        $('.main-nav').slideToggle(200)
+    })
+})
